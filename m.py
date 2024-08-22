42bc6189fac79abd25de0c3937dc866f04b89da5
UDAYTANSHU <gji86062@gmail.com>
2024-08-22 14:59:18 +0530

    Update m.py

--- m.py
+++ m.py
@@ -453,4 +453,13 @@ while True:
     except Exception as e:
         print(e)
 
+if name == 'main':
+    while True:
+        try:
+            bot.polling(non_stop=True, interval=0)
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+            continue
+
 
